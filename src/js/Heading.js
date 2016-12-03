adf0da954f8d8a890b84887c76ee4c8038737751
Ryan Cruz <ryanjosephcruz1@gmail.com>
2016-12-03 09:04:32 +0800

    Major overhaul
    Added Panels and Wells for recommendations and header.
    Updated background images.
    Some CSS tweaks on coloring and size adaptation.
    Removed old images.

--- src/js/Heading.js
+++ src/js/Heading.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Panel, Image } from 'react-bootstrap'
 import '../css/Styles.css'
 import photo from '../img/personal_photo.jpg'
 
@@ -9,9 +10,12 @@ class Heading extends Component {
 			<div>
 				<header className="header">
 					<div className="text-vertical-center cover-photo">
-						<h1>Ryan Cruz</h1>
-						<h3>Programmer Analyst</h3>
-						<img src={photo} className="circle-photo" alt="circle-personal"/>
+						<Panel className="panel-header">
+							<h1>Ryan Cruz</h1>
+							<h3>Programmer Analyst</h3>
+							
+							<Image src={photo} className="cover-photo" circle responsive/>
+						</Panel>
 					</div>
 				</header>
 			</div>
