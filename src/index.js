b32687cc339a7db6ca04c54dc9ce7827a04d995a
Ryan Cruz <ryanjosephcruz1@gmail.com>
2017-02-05 07:33:34 +0800

    Adding react-ga to track pageviews [BETA]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import App from './App'
 import './index.css'
 
+ReactGA.initialize('UA-91402482-1', {
+  debug: true,
+  titleCase: false}
+  )
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+
 ReactDOM.render(
-  <App />,
+  <App onLoad={logPageView()}/>,
   document.getElementById('root')
 );
